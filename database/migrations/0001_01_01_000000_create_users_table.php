620ab192d145aa0447ea7fecab128cd690b9453a
Revanzza <vanzvanza@gmail.com>
2025-05-13 20:50:53 +0700

    rename class for username to name on user table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('username');
+            $table->string('name');
             $table->timestamps();
         });
 
