a3636bddfa708f446a5dd76daa9bb53e09bc9454
Revanzza <vanzvanza@gmail.com>
2025-05-27 13:45:52 +0700

    add slug for information and agenda

--- app/Models/Agenda.php
+++ app/Models/Agenda.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 
 class Agenda extends Model
 {
@@ -16,10 +17,27 @@ class Agenda extends Model
         'tempat',
         'foto_agenda',
         'id_user',
+        'slug', // tambahkan slug di sini juga
     ];
 
     public function user()
     {
         return $this->belongsTo(User::class, 'id_user');
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->id_user = auth()->id(); // otomatis set user yang login
+            $model->slug = \Str::slug($model->nama_agenda) . '-' . uniqid(); // buat slug dari nama_agenda
+        });
+    }
+
+    // Opsional, supaya bisa pakai route model binding via slug
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 }
